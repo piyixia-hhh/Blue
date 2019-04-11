bebd401e462d0a766310e319d2fd93d0a0840ec6
Maps <41610350+piyixia-hhh@users.noreply.github.com>
2019-04-11 15:28:01 +0800

    Add files via upload

---
+++ Test 1/Q.py
@@ -0,0 +1,11 @@
+a =eval(input("请输入a："))
+#此处输入的格式为字符，需要用eval()函数转化为数字才能比较大小
+if a == 0:
+    print("Hello World")
+elif a>0:
+    print("He\nll\no \nWo\nrl\nd")
+elif a<0:
+    print("H\ne\nl\nl\no\n \nW\no\nr\nl\nd")
+
+x =eval(input())
+print("x")
